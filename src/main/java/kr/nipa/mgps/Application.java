ad777d43dbd761ba0de0258710935078eb98ca4d
bcbae <bcbae@gaia3d.com>
2018-08-27 13:31:23 +0900

    Init project

---
+++ src/main/java/kr/nipa/mgps/Application.java
@@ -0,0 +1,12 @@
+package kr.nipa.mgps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
