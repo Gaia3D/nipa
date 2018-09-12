650338cb100e3d0c3151be71363a29c5014fa520
bcbae <bcbae@gaia3d.com>
2018-09-12 20:48:42 +0900

    멀티 프로젝트 구성

---
+++ nipa-admin/src/main/java/kr/nipa/mgps/NipaAdminApplication.java
@@ -0,0 +1,12 @@
+package kr.nipa.mgps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NipaAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NipaAdminApplication.class, args);
+	}
+}
---
+++ nipa-user/src/main/java/kr/nipa/mgps/NipaUserApplication.java
@@ -0,0 +1,12 @@
+package kr.nipa.mgps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NipaUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NipaUserApplication.class, args);
+	}
+}
