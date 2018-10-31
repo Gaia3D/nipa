908e89206a07ac7842b7e0fe41dbde859ece1c98
hjchoi <hjchoi@gaia3d.com>
2018-10-31 17:13:53 +0900

    spring boot initializer 추가

--- nipa-user/src/main/java/kr/nipa/mgps/NipaUserApplication.java
+++ nipa-user/src/main/java/kr/nipa/mgps/NipaUserApplication.java
@@ -2,9 +2,10 @@ package kr.nipa.mgps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NipaUserApplication {
+public class NipaUserApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NipaUserApplication.class, args);
