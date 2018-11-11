c84afa36de7819640809deccb46e1636b16536e5
hjchoi <hjchoi@gaia3d.com>
2018-11-11 14:56:49 +0900

    domain 추가

--- nipa-user/src/main/java/kr/nipa/mgps/domain/CountryPlaceNumber.java
+++ nipa-user/src/main/java/kr/nipa/mgps/domain/CountryPlaceNumber.java
@@ -8,6 +8,35 @@ import lombok.ToString;
 @Setter
 @Getter
 public class CountryPlaceNumber extends SearchFilter {
-
+	
 	private String fullTextSearch;
+	
+	// 고유번호
+	private long gid;
+	// 지점 번호 위치
+	private String geom;
+	// 시군구 코드
+	private String sgg_cd;
+	// 읍면동 코드
+	private String emd_cd;
+	// 리 코드
+	private String li_cd;
+	// 지점 순번
+	private Long spo_fcl_cd;
+	// 지점 번호 코드(통합검색 키워드)
+	private String spo_no_cd;
+	// 지점명 코드 순번
+	private String ins_fcl_sn;
+	// 지점명(통합검색 키워드)
+	private String fcltylc_nm;
+	// GRS80 X
+	private Double grs80_x;
+	// GRS80 Y
+	private Double grs80_y;
+	// 경도
+	private Double lon;
+	// 위도
+	private Double lat;
+	// 갱신일
+	private String spo_ins_date;
 }
--- nipa-user/src/main/java/kr/nipa/mgps/domain/District.java
+++ nipa-user/src/main/java/kr/nipa/mgps/domain/District.java
@@ -10,4 +10,28 @@ import lombok.ToString;
 public class District extends SearchFilter {
 
 	private String fullTextSearch;
+	
+	/******** 시도 **********/
+	private Integer sido_id;
+	private String sido_geom;
+	private String sido_cd;
+	private String sido_eng_nm;
+	private String sido_kor_nm;
+
+	
+	/******** 시군구 **********/
+	private Integer sgg_id;
+	private String sgg_geom;
+	private String sgg_cd;
+	private String sgg_eng_nm;
+	private String sgg_kor_nm;
+	
+	
+	/******** 읍면동 **********/
+	private Integer emd_id;
+	private String emd_geom;
+	private String emd_cd;
+	private String emd_eng_nm;
+	private String emd_kor_nm;
+	
 }
